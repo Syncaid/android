d1837574c5ab483bfbe0a7bf49d1cb1bf054d463
Ahmed Kazez <ahmedgazez@gmail.com>
2023-05-06 23:59:19 +0100

    stats

--- app/src/main/java/com/example/syncaid/view/patient/patientHomeFragment.kt
+++ app/src/main/java/com/example/syncaid/view/patient/patientHomeFragment.kt
@@ -1,11 +1,16 @@
 package com.example.syncaid.view.patient
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.syncaid.R
+import com.example.syncaid.databinding.FragmentPatientHomeBinding
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +26,10 @@ class patientHomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var sleepCycle: BarChart
+    private lateinit var heartRate: LineChart
+    private lateinit var oxygenRate: LineChart
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,14 +37,122 @@ class patientHomeFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-    }
 
+
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_patient_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_patient_home, container, false)
+        sleepCycle = view.findViewById(R.id.sleepcycle)
+        heartRate = view.findViewById(R.id.heartrate)
+        oxygenRate = view.findViewById(R.id.oxygenrate)
+
+        sleepCycle.setDrawGridBackground(false)
+        sleepCycle.setDrawBarShadow(false)
+        sleepCycle.xAxis.setDrawAxisLine(false)
+        sleepCycle.xAxis.setEnabled(false)
+        sleepCycle.axisRight.setEnabled(false);
+        sleepCycle.axisLeft.setEnabled(false);
+        sleepCycle.description = null
+
+        heartRate.setDrawGridBackground(false)
+        heartRate.description = null
+        heartRate.axisLeft.setDrawAxisLine(false)
+        heartRate.axisRight.setDrawAxisLine(false)
+        heartRate.xAxis.setDrawAxisLine(false)
+        heartRate.xAxis.setEnabled(false)
+        heartRate.axisRight.setEnabled(false);
+        heartRate.axisLeft.setEnabled(false);
+
+        oxygenRate.setDrawGridBackground(false)
+        oxygenRate.axisLeft.setDrawAxisLine(false)
+        oxygenRate.xAxis.setDrawAxisLine(false)
+        oxygenRate.xAxis.setEnabled(false)
+        oxygenRate.axisRight.setDrawAxisLine(false)
+
+        oxygenRate.axisRight.setEnabled(false);
+        oxygenRate.axisLeft.setEnabled(false);
+        oxygenRate.description = null
+
+        val heartratesEntries = listOf(
+            Entry(0f, 60f),
+            Entry(1f, 78f),
+            Entry(2f, 85f),
+            Entry(3f, 77f),
+            Entry(4f, 80f),
+            Entry(5f, 70f),
+            Entry(6f, 81f),
+            Entry(7f, 72f),
+            Entry(8f, 68f),
+            Entry(9f, 60f),
+            Entry(10f, 60f),
+        )
+
+        val  OxygenRateEntries= listOf(
+            Entry(0f, 5f),
+            Entry(1f, 10f),
+            Entry(2f, 10f),
+            Entry(3f, 8f),
+            Entry(4f, 6f),
+            Entry(5f, 8f),
+            Entry(6f, 10f),
+            Entry(7f, 5f),
+            Entry(8f, 9f),
+            Entry(9f, 5f),
+            Entry(10f, 3f),
+        )
+        val sleepCycleEntries = listOf(
+            BarEntry(0f, 5f),
+            BarEntry(1f, 5f),
+            BarEntry(2f, 8f),
+            BarEntry(3f, 7f),
+            BarEntry(4f, 6f),
+            BarEntry(5f, 7f),
+            BarEntry(6f, 4f),
+            BarEntry(7f, 9f),
+        )
+
+        val heartdataSet = LineDataSet(heartratesEntries, "")
+        val heartlineData = LineData(heartdataSet)
+        heartdataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+        heartdataSet.color = Color.parseColor("#ee5253")
+        heartdataSet.setCircleColor(Color.parseColor("#ee5253"))
+        heartdataSet.valueTextSize = 10f
+        heartdataSet.lineWidth = 3f
+        heartdataSet.circleRadius = 4f
+
+
+        val oxygendataSet = LineDataSet(OxygenRateEntries, "")
+        val oxygenratedata = LineData(oxygendataSet)
+        oxygendataSet.color = Color.parseColor("#2e86de")
+        oxygendataSet.valueTextColor = Color.GRAY
+        oxygendataSet.valueTextSize = 10f
+        oxygendataSet.lineWidth = 3f
+        oxygendataSet.circleRadius = 4f
+        oxygendataSet.setDrawCircleHole(false)
+        oxygendataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+        oxygendataSet.setCircleColor(Color.parseColor("#2e86de"))
+        oxygendataSet.setDrawHorizontalHighlightIndicator(false)
+
+
+        val barDataSet = BarDataSet(sleepCycleEntries, "")
+        val barData = BarData(barDataSet)
+        barDataSet.valueTextSize = 10f
+        barDataSet.color = Color.parseColor("#f1c40f")
+
+
+        sleepCycle.data = barData
+        sleepCycle.invalidate()
+
+        heartRate.data = heartlineData
+        heartRate.invalidate()
+        oxygenRate.data = oxygenratedata
+        oxygenRate.invalidate()
+
+
+        return view
     }
 
     companion object {
